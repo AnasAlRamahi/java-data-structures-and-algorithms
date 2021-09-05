6a0cb5b64b34c978b15459c8b0118b70ae424d52
AnasAlRamahi <anas_alramahi@outlook.com>
2021-09-05 08:47:43 +0300

    the right idea but has an error nullpointerexception

---
+++ hashTable/lib/src/main/java/hashTable/Node.java
@@ -0,0 +1,18 @@
+package hashTable;
+
+public class Node <T>{
+    private T value;
+    Node next;
+
+    public Node(T value) {
+        this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+}
